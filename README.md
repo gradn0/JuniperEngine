a459ff7bc5d23af5718592d755dd1d9964ad6abf
gradn0 <nathan.gradden@outlook.com>
2025-07-15 13:17:17 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Juniper Engine
+
+An in-progress 2D game engine written in C++.
+
+# Building
+
+1. Clone the repository
+
+```
+git clone --recurse-submodules https://github.com/gradn0/JuniperEngine.git
+```
+
+2. Run CreateProject.bat to generate a Visual Studio 2022 solution
+3. Build the solution inside Visual Studio
---
+++ premake5.lua
@@ -0,0 +1,147 @@
+outdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+workspace "Juniper"
+    architecture "x64"
+    startproject "JuniperEditor"
+    configurations { "Debug", "Release" }
+
+group "Vendor"
+    include "Juniper/vendor/glfw"
+    include "Juniper/vendor/glad"
+    include "Juniper/vendor/imgui"
+
+group ""
+
+project "Juniper"
+    location "Juniper"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "on"
+
+    targetdir ("bin/" .. outdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outdir .. "/%{prj.name}")
+
+    pchheader "pch.h"
+    pchsource "%{prj.name}/src/pch.cpp"
+
+    files {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp",
+        "%{prj.name}/vendor/glad/src/glad.c",
+        "%{prj.name}/vendor/stb_image/**.h", "%{prj.name}/vendor/stb_image/**.cpp"
+    }
+
+    includedirs {
+        "%{prj.name}/src",
+        "%{prj.name}/vendor/spdlog/include",
+        "%{prj.name}/vendor/glfw/include",
+        "%{prj.name}/vendor/glad/include",
+        "%{prj.name}/vendor/imgui",
+        "%{prj.name}/vendor/glm",
+        "%{prj.name}/vendor/stb_image",
+        "%{prj.name}/vendor/entt",
+    }
+
+    links { "GLFW", "glad", "opengl32.lib", "ImGui" }
+
+    -- Systems
+    filter "system:windows"
+        systemversion "latest"
+        defines { "MP_PLATFORM_WINDOWS", "MP_BUILD_DLL" }
+        buildoptions { "/utf-8" }
+
+    -- Configurations
+    filter "configurations:Debug"
+        defines { "MP_DEBUG", "MP_ASSERTS", "GLFW_INCLUDE_NONE" }
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        defines { "MP_RELEASE", "GLFW_INCLUDE_NONE" } 
+        runtime "Release"
+        optimize "on"
+
+    -- Files
+    filter "files:Juniper/vendor/glad/src/glad.c"
+        flags { "NoPCH" }
+
+project "Application"
+    location "Application"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "on"
+
+    targetdir ("bin/" .. outdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outdir .. "/%{prj.name}")
+
+    files { "%{prj.name}/src/**.h", "%{prj.name}/src/**.cpp" }
+
+    includedirs {
+        "Juniper/vendor/spdlog/include",
+        "Juniper/vendor/glfw/include",
+        "Juniper/vendor/glad/include",
+        "Juniper/src",
+        "Juniper/vendor/glm",
+        "Juniper/vendor/entt",
+        "Juniper/vendor"
+    }
+
+    links { "Juniper" }
+
+    filter "system:windows"
+        staticruntime "On"
+        systemversion "latest"
+        defines { "MP_PLATFORM_WINDOWS", "GLFW_INCLUDE_NONE" }
+        buildoptions { "/utf-8" }
+
+    filter "configurations:Debug"
+        defines { "MP_DEBUG", "MP_ASSERTS" }
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        defines { "MP_RELEASE", "GLFW_INCLUDE_NONE" }
+        runtime "Release"
+        optimize "on"
+
+project "JuniperEditor"
+    location "JuniperEditor"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "on"
+
+    targetdir ("bin/" .. outdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outdir .. "/%{prj.name}")
+
+    files { "%{prj.name}/src/**.h", "%{prj.name}/src/**.cpp" }
+
+    includedirs {
+        "Juniper/vendor/spdlog/include",
+        "Juniper/vendor/glfw/include",
+        "Juniper/vendor/glad/include",
+        "Juniper/src",
+        "Juniper/vendor/glm",
+        "Juniper/vendor/entt",
+        "Juniper/vendor"
+    }
+
+    links { "Juniper" }
+
+    filter "system:windows"
+        staticruntime "On"
+        systemversion "latest"
+        defines { "MP_PLATFORM_WINDOWS", "GLFW_INCLUDE_NONE" }
+        buildoptions { "/utf-8" }
+
+    filter "configurations:Debug"
+        defines { "MP_DEBUG", "MP_ASSERTS" }
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        defines { "MP_RELEASE", "GLFW_INCLUDE_NONE" }
+        runtime "Release"
+        optimize "on"
